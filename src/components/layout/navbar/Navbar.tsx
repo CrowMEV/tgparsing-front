f924a8406ec035b81c7d47fe1ccb7afacfdf31fd
andrei110295@yandex.ru <andrei110295@yandex.ru>
2023-07-02 18:37:41 +0300

    fixed navbar and dashboard

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -22,43 +22,48 @@ const Navbar = ({ menuItems, currentPage }: PropTypes) => {
       : -1;
 
   return (
-    <div className={styles.nav}>
-      <ReplenishmentWindow
-        isActive={isReplenishmentOpen}
-        setActive={setIsReplenishmentOpen}
-      />
-      <nav className={styles.nav__links}>
-        <div className={styles.nav__list}>
-          <NavTabs
-            currentElementIndex={currentLinkPosition}
-            underlineClass={styles.nav__underline}
-          >
-            {menuItems.map((item, index) => (
-              <Link
-                to={item.link}
-                className={`${styles.nav__link} ${
-                  index === currentLinkPosition ? styles.nav__link_active : ''
-                } main-nav__link`}
-                key={item.text}
-              >
-                {item.icon && (
-                  <div className={styles.nav__icon}>{item.icon}</div>
-                )}
-                <span>{item.text}</span>
-              </Link>
-            ))}
-          </NavTabs>
-        </div>
-        <div>
-          <div className={styles.nav__links__balance}>
-            <p className={styles.balance__title}>Ваш баланс:</p>
-            <p className={styles.balance__value}>2000.00 &#8381;</p>
+    <div className={styles.wrapper}>
+      <div className={styles.nav}>
+        <ReplenishmentWindow
+          isActive={isReplenishmentOpen}
+          setActive={setIsReplenishmentOpen}
+        />
+        <nav className={styles.nav__links}>
+          <div className={styles.nav__list}>
+            <NavTabs
+              currentElementIndex={currentLinkPosition}
+              underlineClass={styles.nav__underline}
+            >
+              {menuItems.map((item, index) => (
+                <Link
+                  to={item.link}
+                  className={`${styles.nav__link} ${
+                    index === currentLinkPosition ? styles.nav__link_active : ''
+                  } main-nav__link`}
+                  key={item.text}
+                >
+                  {item.icon && (
+                    <div className={styles.nav__icon}>{item.icon}</div>
+                  )}
+                  <span>{item.text}</span>
+                </Link>
+              ))}
+            </NavTabs>
           </div>
-          <Button variant="small" onClick={() => setIsReplenishmentOpen(true)}>
-            Пополнить
-          </Button>
-        </div>
-      </nav>
+          <div>
+            <div className={styles.nav__links__balance}>
+              <p className={styles.balance__title}>Ваш баланс:</p>
+              <p className={styles.balance__value}>2000.00 &#8381;</p>
+            </div>
+            <Button
+              variant="small"
+              onClick={() => setIsReplenishmentOpen(true)}
+            >
+              Пополнить
+            </Button>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
